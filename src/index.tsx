0a0002d763b1596ad5ab0a1b23d2e3a43f0e8243
luiggy macias <ferrinluiggy@gmail.com>
2022-02-20 15:50:35 -0500

    learning usen button

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import { render } from "react-dom";
+import { DatePicker, message, Alert, Button } from "antd";
+import "antd/dist/antd.css";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+  const [date, setDate] = useState<any>(null);
+  const [show, setShow] = useState<boolean>(false);
+  const handleChange = (value: any) => {
+    console.log(value);
+    message.info(
+      `Selected Date: ${value ? value.format("YYYY-MM-DD") : "None"}`
+    );
+    setDate(value);
+    setShow(true);
+  };
+  return (
+    <div style={{ width: 400, margin: "100px auto" }}>
+      <DatePicker onChange={handleChange} />
+      <Alert
+        style={{
+          display: show ? "block" : "none",
+        }}
+        message="Selected date"
+        description={date ? date.format("YYYY-MM-DD") : "None"}
+      />
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+      <Button onClick={() => setShow(false)} type="primary">
+        Hide date
+      </Button>
+      <Button>Default Button</Button>
+      <Button onClick={() => setShow(true)} type="dashed">
+        Show date
+      </Button>
+      <br />
+      <Button type="text">Text Button</Button>
+      <Button type="link">Link Button</Button>
+    </div>
+  );
+};
+
+render(<App />, document.getElementById("root"));
